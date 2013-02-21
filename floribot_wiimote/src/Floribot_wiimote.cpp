5ff4cfae2e07551b0fd5c8f94a4d765ef4716374
thever <Torsten.Heverhagen@hs-heilbronn.de>
2013-02-21 15:47:43 +0100

    commit during meeting

--- floribot_wiimote/src/Floribot_wiimote.cpp
+++ floribot_wiimote/src/Floribot_wiimote.cpp
@@ -14,15 +14,22 @@ namespace floribot_wiimote {
 
 Floribot_wiimote::Floribot_wiimote(ros::NodeHandle n) : n_(n)
 {
-	cmd_vel_pub = n_.advertise<geometry_msgs::Twist>("cmd_vel",1);
-	joy_set_feedback_pub = n_.advertise<sensor_msgs::JoyFeedbackArray>("joy/set_feedback",1);
 	joy_sub = n_.subscribe("joy", 1,
 			&Floribot_wiimote::joy_message, this);
+	cmd_vel_pub = n_.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	joy_set_feedback_pub = n_.advertise<sensor_msgs::JoyFeedbackArray>("joy/set_feedback",1);
 } // end of constructor
 
 Floribot_wiimote::~Floribot_wiimote()
 {
 	// Start of user code destructure
+		// TODO: fill with your code
+	// End of user code
+}
+
+void Floribot_wiimote::joy_message (const sensor_msgs::Joy::ConstPtr& msg)
+{
+	// Start of user code process message
 		// TODO: fill with your code
 	// End of user code
 }
@@ -35,13 +42,6 @@ void Floribot_wiimote::publish_cmd_vel (const geometry_msgs::Twist::ConstPtr& ms
 void Floribot_wiimote::publish_joy_set_feedback (const sensor_msgs::JoyFeedbackArray::ConstPtr& msg)
 {
 	joy_set_feedback_pub.publish(msg);
-}
-
-void Floribot_wiimote::joy_message (const sensor_msgs::Joy::ConstPtr& msg)
-{
-	// Start of user code process message
-		// TODO: fill with your code
-	// End of user code
 }
 
 void Floribot_wiimote::tick ()
--- floribot_wiimote/src/Floribot_wiimote.h
+++ floribot_wiimote/src/Floribot_wiimote.h
@@ -9,9 +9,9 @@
 #define FLORIBOT_WIIMOTE_H_
 
 #include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/JoyFeedbackArray.h>
-#include <sensor_msgs/Joy.h>
 
 namespace floribot_wiimote {
 
@@ -22,15 +22,15 @@ public:
 	virtual ~Floribot_wiimote();
 	void tick();
 
+	void joy_message (const sensor_msgs::Joy::ConstPtr& msg);
 	void publish_cmd_vel (const geometry_msgs::Twist::ConstPtr& msg);
 	void publish_joy_set_feedback (const sensor_msgs::JoyFeedbackArray::ConstPtr& msg);
-	void joy_message (const sensor_msgs::Joy::ConstPtr& msg);
 
 private:
 	ros::NodeHandle n_;
+	ros::Subscriber joy_sub;
 	ros::Publisher cmd_vel_pub;
 	ros::Publisher joy_set_feedback_pub;
-	ros::Subscriber joy_sub;
 	// Start of user code additional members
 		// TODO: declare your variables and methods
 	// End of user code
