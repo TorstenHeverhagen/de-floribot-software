6afe69060f7bcad5dff4b5a4bdf43f76c7b199e5
TorstenHeverhagen <thever@floribot2>
2013-06-13 23:31:55 +0200

    merging

--- floribot_wiimote/src/Floribot_wiimote.h
+++ floribot_wiimote/src/Floribot_wiimote.h
@@ -20,21 +20,6 @@
 
 namespace floribot_wiimote {
 
-// Enumeration of available control modes
-enum ControlMode {
-	NO_CONTROL,
-	MANUAL,
-	TASK_1_SET,
-	TASK_1_START,
-	TASK_2,
-	TASK_3,
-	TASK_4,
-	TASK_5
-};
-
-// Type for control modes
-typedef ControlMode ControlMode_t;
-
 class Floribot_wiimote 
 {
 public:
@@ -43,40 +28,29 @@ public:
 	void tick();
 	int get_tick_rate();
 
-	void publish_cmd_vel (geometry_msgs::Twist msg);
-	void joy_message (const sensor_msgs::Joy::ConstPtr& msg);
 	void task_cmd_vel_message (const geometry_msgs::Twist::ConstPtr& msg);
+	void joy_message (const sensor_msgs::Joy::ConstPtr& msg);
+	void publish_cmd_vel (geometry_msgs::Twist msg);
 	void publish_joy_set_feedback (sensor_msgs::JoyFeedbackArray msg);
 
+	// Start of user code additional public members
+	// TODO: declare your variables and methods
+	// End of user code  don't delete this line
 private:
 	ros::NodeHandle n_;
 	int tick_rate;
-	ros::Publisher cmd_vel_pub;
-	ros::Subscriber joy_sub;
 	ros::Subscriber task_cmd_vel_sub;
+	ros::Subscriber joy_sub;
+	ros::Publisher cmd_vel_pub;
 	ros::Publisher joy_set_feedback_pub;
 	// Start of user code additional members
 
 	geometry_msgs::Twist joy_to_vel();
+	sensor_msgs::JoyFeedbackArray prepare_joy_feedback();
 
-	sensor_msgs::Joy joy_msg;
-	ControlMode_t actual_state;
-	ControlMode_t next_state;
 	bool new_msg;
 
 	geometry_msgs::Twist task1_vel;
-	/* ---Constants--- */
-
-	/* Movement control parameters */
-
-	// divisor for pitch axis value in full accelerometer control mode
-	static const int DIRECT_PITCH_AXIS_DIV = 10;
-	// divisor for roll axis value in full accelerometer control mode
-	static const int DIRECT_ROLL_AXIS_DIV = 30;
-	// divisor for pitch axis value in button-controlled accelerometer mode
-	static const int BUTTON_PITCH_AXIS_DIV = 20;
-	// divisor for roll axis value in button-controlled accelerometer mode
-	static const int BUTTON_ROLL_AXIS_DIV = 20;
 
 	// End of user code  don't delete this line
 };
