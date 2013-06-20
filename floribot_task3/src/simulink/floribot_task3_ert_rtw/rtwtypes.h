a8ae6f286d4c160105cecc5a27d0c02c7f57b74d
thever <Torsten.Heverhagen@hs-heilbronn.de>
2013-06-20 16:41:04 +0200

    wiimote reverted

--- floribot_task3/src/simulink/floribot_task3_ert_rtw/rtwtypes.h
+++ floribot_task3/src/simulink/floribot_task3_ert_rtw/rtwtypes.h
@@ -3,10 +3,10 @@
  *
  * Code generated for Simulink model 'floribot_task3'.
  *
- * Model version                  : 1.69
+ * Model version                  : 1.72
  * Simulink Coder version         : 8.4 (R2013a) 13-Feb-2013
  * TLC version                    : 8.4 (Jan 19 2013)
- * C/C++ source code generated on : Wed Jun 19 01:26:32 2013
+ * C/C++ source code generated on : Thu Jun 20 15:37:02 2013
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: 32-bit Generic
